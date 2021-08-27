9d5352ffccb17786b77b43c0ecc8589e4955bfb8
miralis221 <65534702+miralis221@users.noreply.github.com>
2021-08-27 20:56:16 +0200

    Correcciones
    * Se ha corregido el array de los datos de entrada en grados fahrenheit.
    
    *Se ha cambiado el metodo de importar la libreria pyplot

--- celsiusToFarenheit.py
+++ celsiusToFarenheit.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 celsius = np.array([-40, -10, 0, 8, 15,22, 38])
-fahrenheit = np.array([-40,14,32,46,59,72,100])
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100])
 
 capa = tf.keras.layers.Dense(units = 1, input_shape=[1])
 modelo =  tf.keras.Sequential([capa])
@@ -16,13 +16,14 @@ print("Comenzando entrenamiento")
 historial = modelo.fit(celsius,fahrenheit,epochs=1000, verbose = False)
 print("Modelo entrenado!!")
 
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 plt.xlabel("# Epoca")
 plt.ylabel("Magnitud de pérdida")
 plt.plot(historial.history["loss"])
+plt.show()
 
 print("Hagamos una prediccion!")
-resultado = modelo.predict([100.0])
+resultado = modelo.predict([50.0])
 print("El resultado es "+str(resultado) + " fahrenheit!")
 
 
